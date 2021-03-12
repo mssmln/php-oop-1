1e07fa36c1f362d810ad7805f02315e73b0f9d28
Massimiliano <m.croci.ly@gmail.com>
2021-03-12 13:56:45 +0100

    init

---
+++ index.php
@@ -0,0 +1,6 @@
+/*● Creazione di una classe ‘Movie’
+● Dichiarazione delle proprietà della classe
+● Dichiarazione del costruttore
+● Dichiarazione di almeno un metodo.
+● Fare poi degli esempi di utilizzo della classe istanziando almeno due oggetti e stampando a schermo il
+valore delle proprietà.*/
